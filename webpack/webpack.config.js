6088bdec618bb65cf4105588d62a6cfa4a773922
wallacemkk <wallaceedua@gmail.com>
2019-11-04 00:15:43 -0300

    Atualizado Capítulo de Webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const modoDev = process.env.NODE_ENV != 'production'
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+    mode: modoDev ? 'development' : 'production',
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ]
+    ,
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                    'css-loader', // Interpreta @importa, url() etc...
+                    'sass-loader',
+                ]
+            }
+        ]
+    }
+}
